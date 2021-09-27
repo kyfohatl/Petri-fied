d626d3c5594bf1bf0f54cfdab56361784f5af4d5
Bodmas12 <80082894+Bodmas12@users.noreply.github.com>
2021-09-27 16:31:47 +1000

    Refactoring of player and enemies complete. MattTestScene also added to main

--- Petri-fied/Assets/Scripts/Enemy.cs
+++ Petri-fied/Assets/Scripts/Enemy.cs
@@ -2,81 +2,140 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
-public class Enemy : MonoBehaviour
+public class Enemy : IntelligentAgent
 {
-    // Enemy data elements
-    public int Score;
-    public string Name;
-    public float Radius; // derivable radius
-    public float GrowthRate = 1f;// growth rate: when eating food
+  // Target and determination delay
+  public Transform Target;
+  
+  // Track player
+  public GameObject Player;
+  
+  // Radius of player visibility
+  public float LookRadius;
+  public float LookRadiusMultiplier = 5f;
+  
+  // Function to call parent contructor
+  public Enemy(string givenName) : base(givenName)
+  {
+    Debug.Log("Enemy: " + Name + " has been generated into the game.");
+  }
+  
+  // Start is called before the first frame update
+  void Start()
+  {
+    GenerateRandomName();
+    Player = GameObject.FindGameObjectWithTag("Player");
+    LookRadius = LookRadiusMultiplier * Radius;
+    InvokeRepeating("DecayScore", 10.0f, 30.0f); // first call: 10s, repeats: 30s
+  }
+  
+  // Update is called once per frame
+  void Update()
+  {
+    UpdateSize();
     
-    // Start is called before the first frame update
-    void Start()
+    if (Target == null)
     {
-        Radius = Mathf.Sqrt(Score);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float increment = 0.01f;
-        float sizeDifference = transform.localScale.x - Radius;
-        
-        if (sizeDifference <= 0)
-        {
-            transform.localScale += new Vector3(increment, increment, increment);
-        }
-        else if (sizeDifference > 0 && sizeDifference > increment)
-        {
-            transform.localScale -= new Vector3(increment, increment, increment);
-        }
-    }
-    
-    // Function to update Score: called by collision events
-    public void UpdateScore(int amount)
-    {
-        Score += amount;
-        Radius = Mathf.Sqrt(Score);
-    }
-    
-    // Function called on collisions
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Food")
-        {
-            int increase = (int)Mathf.Ceil(GrowthRate);
-            UpdateScore(increase);
-            Destroy(other.gameObject);
-        }
-        else if (other.gameObject.tag == "Enemy")
-        {
-            if (other.gameObject.GetComponent("Enemy") != null)
-            {
-                Enemy otherPlayer = other.gameObject.GetComponent<Enemy>();
-                int scoreDifference = Score - otherPlayer.getScore();
-                if (scoreDifference > 0)
-                {
-                    UpdateScore(scoreDifference);
-                    Destroy(other.gameObject);
-                }
-                else if (scoreDifference < 0)
-                {
-                    Destroy(gameObject);
-                }
-            }
-        }
+      DetermineTarget();
     }
     
-    // Getter method for score
-    public int getScore()
+    if (Target != null)
     {
-        return Score;
+      FaceTarget();
+      transform.position += 2.0f * transform.forward * this.SpeedMultiplier * Time.deltaTime / transform.localScale.x;
+    }
+  }
+  
+  // Override Function to update radius and also look radius
+  public override void UpdateRadius()
+  {
+    base.UpdateRadius();
+    this.LookRadius = this.LookRadiusMultiplier * Radius;
+  }
+  
+  // Function to generate a random name
+  private void GenerateRandomName()
+  {
+    string randomString = "";
+    int digitCount = 3;
+    
+    randomString += (char)UnityEngine.Random.Range('A', 'Z');
+    for (int i = 0; i < digitCount; i++)
+    {
+      randomString += (char)UnityEngine.Random.Range('0', '9');
+    }
+    randomString += "-" + (char)UnityEngine.Random.Range('A', 'Z');
+    
+    this.Name = randomString;
+  }
+  
+  // Function to determine target
+  public void DetermineTarget()
+  {
+    GameObject closestEnemy = null;
+    GameObject closestFood = null;
+    
+    float playerDistance = Vector3.Distance(transform.position, Player.transform.position);
+    int playerScore = Player.GetComponent<Player>().getScore();
+    
+    // Prioritise player first
+    if (playerDistance <= LookRadius && playerScore < this.getScore())
+    {
+      // Player has lower score and within 5 radius.
+      this.Target = Player.transform;
+      return;
     }
     
-    // Getter method for radius
-    public float getRadius()
+    // Secondly, other enemies
+    closestEnemy = GetClosestObject(GameManager.get().getEnemies());
+    if (closestEnemy != null)
     {
-        return Radius;
+      float enemyDistance = Vector3.Distance(transform.position, closestEnemy.transform.position);
+      int enemyScore = closestEnemy.GetComponent<Enemy>().getScore();
+      
+      if (enemyDistance <= this.LookRadius && enemyScore < this.getScore())
+      {
+        // Another enemy has lower score and within 5 radius.
+        this.Target = closestEnemy.transform;
+        return;
+      }
     }
+    
+    // Lastly, food
+    closestFood = GetClosestObject(GameManager.get().getFood());
+    if (closestFood != null)
+    {
+      this.Target = closestFood.transform;
+    }
+  }
+  
+  // Rotate enemy towards at target
+  void FaceTarget()
+  {
+    Vector3 direction = (Target.position - transform.position).normalized;
+    Quaternion lookRotation = Quaternion.LookRotation(direction);
+    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 1f);
+  }
+  
+  // Find nearest object in a dictionary
+  GameObject GetClosestObject(Dictionary<int, GameObject> objs)
+  {
+    GameObject tMin = null;
+    float minDist = Mathf.Infinity;
+    Vector3 currentPos = transform.position;
+    
+    foreach (KeyValuePair<int, GameObject> t in objs)
+    {
+      float dist = Vector3.Distance(t.Value.transform.position, currentPos);
+      if (dist < minDist)
+      {
+        tMin = t.Value;
+        minDist = dist;
+      }
+    }
+    
+    return tMin;
+  }
 }
--- Petri-fied/Assets/Scripts/FoodSpawn.cs
+++ Petri-fied/Assets/Scripts/FoodSpawn.cs
@@ -4,55 +4,60 @@ using UnityEngine;
 
 public class FoodSpawn : MonoBehaviour
 {
-    // Food prefab
-    public GameObject Food;
+  // Food prefab
+  public GameObject Food;
+  
+  // Spawn rate of food
+  public float spawnRate; // per second
+  private float deltaSpawn; // delay between spawn generations
+  
+  public int spawnMin = 1; // lower bound spawns per generation
+  public int spawnMax = 1; // upper bound spawns per generation
+  public int spawnLimit = 100; // maximum spawns allowed
+  
+  private float timer; // clock to track time between generations
+  
+  // Start is called before the first frame update
+  void Start()
+  {
+    deltaSpawn = 1f / spawnRate;
+    timer = deltaSpawn;
+  }
+  
+  // Update is called once per frame
+  void Update()
+  {
+    timer -= Time.deltaTime;
     
-    // Spawn rate of food
-    public float spawnRate; // per second
-    private float deltaSpawn; // delay between spawn generations
-    
-    public int spawnMin = 1; // lower bound spawns per generation
-    public int spawnMax = 1; // upper bound spawns per generation
-    public int spawnLimit = 100; // maximum spawns allowed
-    
-    private float timer; // clock to track time between generations
-    
-    // Start is called before the first frame update
-    void Start()
+    if (timer < 0 && transform.childCount < spawnLimit)
     {
-        deltaSpawn = 1f / spawnRate;
-        timer = deltaSpawn;
+      timer = deltaSpawn;
+      int spawnCount = Random.Range(spawnMin, spawnMax);
+      
+      // Loop to spawn one or more food objects per generation
+      for (int i = 0; i < spawnCount; i++)
+      {
+        Generate();
+      }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        timer -= Time.deltaTime;
-        
-        if (timer < 0 && transform.childCount < spawnLimit)
-        {
-            timer = deltaSpawn;
-            int spawnCount = Random.Range(spawnMin, spawnMax);
-            
-            // Loop to spawn one or more food objects per generation
-            for (int i = 0; i < spawnCount; i++)
-            {
-                Generate();
-            }
-        }
-    }
+  }
+  
+  // Function to instantiate new food objects into the world
+  void Generate()
+  {
+    // Determine spawn position
+    float radius = 150f;
+    float height = 50f;
     
-    // Function to instantiate new food objects into the scene: needs updating to map to petri-dish dimensions (can be a child of the petri dish to get dimensions from parent is my idea)
-    void Generate()
-    {
-        // Below variables currently spawn food
-        float x = Random.Range(-50, 50);
-        float y = Random.Range(-10, 10);
-        float z = Random.Range(-50, 50);
-
-        Vector3 Target = new Vector3(x, y, z);
-        
-        // Actually generates the food and sets as child of spawner
-        GameObject spawned = Instantiate(Food, Target, Quaternion.identity, transform);
-    }
+    Vector2 coord = Random.insideUnitCircle * radius;
+    float y = Random.Range(transform.position.z - height / 2f, transform.position.z + height / 2f);
+    
+    Vector3 Target = new Vector3(transform.position.x + coord.x, transform.position.y + y, transform.position.z + coord.y);
+    
+    // Actually generates the food and sets as child of spawner
+    GameObject spawned = Instantiate(Food, Target, Random.rotation, transform);
+    
+    // Track the food spawned.
+    GameManager.AddFood(spawned.GetInstanceID(), spawned);
+  }
 }
