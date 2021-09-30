365a56030909100ec1f3cfe41bbe2d083f40be55
Bodmas12 <80082894+Bodmas12@users.noreply.github.com>
2021-09-30 13:30:22 +1000

    Revert "Intelligent agent and children scripts (also includes changes to controller script)"
    This reverts commit 3b8daf3641efda3e07d47e664606b9a9e01b598f, reversing
    changes made to 23956cf4adc537a0cf468de8faefe79b666a7228.

--- Petri-fied/Assets/Scripts/Player.cs
+++ Petri-fied/Assets/Scripts/Player.cs
@@ -7,7 +7,6 @@ using TMPro;
 
 public class Player : IntelligentAgent
 {
-<<<<<<< HEAD
   // Player statistics tracking
   private float survivalTime = 0.0f;
 
@@ -49,44 +48,4 @@ public class Player : IntelligentAgent
 
     this.timeLabel.text = hours.ToString() + ":" + minutes.ToString("D2") + ":" + seconds.ToString("D2");
   }
-=======
-	// Player statistics tracking
-	private float survivalTime = 0.0f;
-	
-	// UI elements
-	public TMP_Text scoreLabel;
-	public TMP_Text nameLabel;
-	public TMP_Text timeLabel;
-	
-	// Start is called before the first frame update
-	void Start()
-	{
-		StartLife();
-		this.nameLabel.text = Name;
-		UpdateGUI();
-	}
-	
-	// Update is called once per frame
-	void Update()
-	{
-		this.survivalTime += Time.deltaTime;
-		DecayScore();
-		UpdateSize();
-		
-		// Lastly, update GUI to reflect updated data
-		UpdateGUI();
-	}
-	
-	// Function to update the GUI
-	void UpdateGUI()
-	{
-		this.scoreLabel.text = Score.ToString();
-		
-		int hours = TimeSpan.FromSeconds(this.survivalTime).Hours;
-		int minutes = TimeSpan.FromSeconds(this.survivalTime).Minutes;
-		int seconds = TimeSpan.FromSeconds(this.survivalTime).Seconds;
-		
-		this.timeLabel.text = hours.ToString() + ":" + minutes.ToString("D2") + ":" + seconds.ToString("D2");
-	}
->>>>>>> shader_2-implementation
 }
--- Petri-fied/Assets/Scripts/PlayerController.cs
+++ Petri-fied/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(IsMoving))]
 public class PlayerController : MonoBehaviour
 {
-<<<<<<< HEAD
   [SerializeField]
   private CharacterController controller;
   [SerializeField]
@@ -53,95 +52,4 @@ public class PlayerController : MonoBehaviour
       gameObject.GetComponent<IsMoving>().isMoving = false;
     }
   }
-=======
-	[SerializeField]
-	private CharacterController controller;
-	[SerializeField]
-	private float speedMultiplier;
-	[SerializeField]
-	private float acceleration = 2f;
-	// Controls how smoothly the player turns from one direction to another
-	[SerializeField]
-	private float turnSmooth = 0.05f;
-	public Transform cam;
-	
-	private Vector3 curDir = new Vector3(0f, 0f, 0f);
-	
-	// Start is called before the first frame update
-	void Start()
-	{
-		this.speedMultiplier = 10f;
-	}
-	
-	// Update is called once per frame
-	void Update()
-	{
-		// Calculate the target direction of movement
-		Vector3 horizontalMoveDir = this.cam.right * Input.GetAxisRaw("Horizontal");
-		Vector3 verticalMoveDir = this.cam.forward * Input.GetAxisRaw("Vertical");
-		Vector3 hoverMoveDir = this.cam.up * Input.GetAxisRaw("Hover");
-		Vector3 targetDir = (horizontalMoveDir + verticalMoveDir + hoverMoveDir).normalized;
-		
-		if (targetDir.magnitude >= 0.1f)
-		{
-			// Player is moving
-			gameObject.GetComponent<IsMoving>().isMoving = true;
-			
-			// Check for lock-on target and calculate direction based on acceleration
-			GameObject curTarget = this.gameObject.GetComponent<Player>().getTarget();
-			if (curTarget == null)
-			{
-				this.curDir = Vector3.Lerp(curDir, targetDir, acceleration * Time.deltaTime);
-			}
-			else
-			{
-				targetDir = (curTarget.transform.position - this.transform.position).normalized;
-				this.curDir = Vector3.Lerp(curDir, targetDir, acceleration * Time.deltaTime);
-			}
-			
-			// Rotate the model to face the direction of travel
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDir), turnSmooth);
-			
-			// Move towards the current direction
-			float geneticSpeedMultiplier = this.gameObject.GetComponent<Player>().getSpeedMultiplier();
-			float finalSpeedMultiplier = this.speedMultiplier * geneticSpeedMultiplier;
-			controller.Move(curDir * finalSpeedMultiplier * Time.deltaTime / transform.localScale.x);
-		}
-		else
-		{
-			// Player is not moving
-			this.gameObject.GetComponent<IsMoving>().isMoving = false;
-		}
-		
-		if (Input.GetMouseButtonDown(0))
-		{
-			RaycastHit hitInfo = new RaycastHit();
-			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
-			
-			if (hit)
-			{
-				string targetTag = hitInfo.transform.gameObject.tag;
-				if (targetTag == "Enemy")
-				{
-					Debug.Log ("Locked-onto: " + hitInfo.transform.gameObject.GetComponent<Enemy>().getName());
-					this.gameObject.GetComponent<Player>().setTarget(hitInfo.transform.gameObject);
-				}
-				else if (targetTag == "Food")
-				{
-					Debug.Log ("Locked-onto: " + hitInfo.transform.gameObject.tag);
-					this.gameObject.GetComponent<Player>().setTarget(hitInfo.transform.gameObject);
-				}
-				else // remove lock-on
-				{
-					Debug.Log ("No longer locked-on!");
-					this.gameObject.GetComponent<Player>().setTarget(null);
-				}
-			}
-			else
-			{
-				Debug.Log("No hit");
-			}
-		}
-	}
->>>>>>> shader_2-implementation
 }
