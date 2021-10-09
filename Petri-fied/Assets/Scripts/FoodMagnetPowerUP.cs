19921d4e9241f7bcace3c6eae657bd2bb43effe5
cgrimwade <cgrimwade@student.unimelb.edu.au>
2021-10-09 15:18:24 +1100

    Food Magnet PowerUp

---
+++ Petri-fied/Assets/Scripts/FoodMagnetPowerUP.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodMagnetPowerUP : MonoBehaviour
+{
+
+    public float MagnetStrength;
+    public GameObject MagnetEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var particleSystem = Instantiate(MagnetEffect, this.transform.position , 
+        this.transform.rotation, this.transform);
+        particleSystem.transform.localScale = this.transform.localScale;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //If hits food, pull food to centre
+    void OnTriggerStay(Collider FoodHit){
+        if(FoodHit.gameObject.tag == "Food"){
+            Vector3 direction = (transform.position - FoodHit.gameObject.transform.position).normalized;
+            FoodHit.gameObject.transform.Translate(direction * MagnetStrength*Time.deltaTime);     
+
+
+        }else{
+            return;
+        }
+
+    }
+}
--- Petri-fied/Assets/Scripts/PowerUpManager.cs
+++ Petri-fied/Assets/Scripts/PowerUpManager.cs
@@ -8,12 +8,28 @@ public class PowerUpManager : MonoBehaviour
     public float SpeedPowerUpMult = 5.0f;
     public float duration = 5f;
     private float BaseSpeedMult = 1f;
+    public float FoodMagnetScale = 5f;
+    public float FoodMagnetSpeed = 0.5f;
     public GameObject SpeedEffect1;//Will call these effect from folder in final product
     public GameObject SpeedEffect2;
+    public GameObject FoodMagnet;
+    private int PowerUpType;
     // Start is called before the first frame update
     void Start()
     {  
-        //pick random effect for the powerup 
+        //pick random effect for the powerup
+        // 0 is Speed PowerUP
+        // 1 is Food Magnet
+        PowerUpType = Random.Range(0,2);
+        //Change the visual of the PowerUp pick-up
+        switch(PowerUpType){
+            case 0:
+                GetComponent<Renderer>().material.color = Color.red; //Temp Color
+                break;
+            case 1:
+                GetComponent<Renderer>().material.color = Color.green; //Temp Color
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -25,20 +41,32 @@ public class PowerUpManager : MonoBehaviour
 
     void OnTriggerEnter(Collider other){
         if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Player"){
-            //Remove PowerUp object
+            //Remove PowerUp object (visually)
             GetComponent<MeshRenderer>().enabled = false;
             GetComponent<Collider>().enabled = false;
-            StartCoroutine( SpeedPowerUp(other));
+
+            //Run power Up code
+            switch(PowerUpType)
+            {
+                case 0:
+                    StartCoroutine( SpeedPowerUp(other));
+                    break;
+                case 1:
+                    StartCoroutine( FoodMagnetPowerUp(other));
+                    break;
+            }
         }else{
             return;
         }
     }
 
+
+    //Code for Speed Power UP
     IEnumerator SpeedPowerUp(Collider other){
 
 
         
-        IntelligentAgent actor = other.gameObject.GetComponent<IntelligentAgent>();;
+        IntelligentAgent actor = other.gameObject.GetComponent<IntelligentAgent>();
 
 
         //Speed power up
@@ -62,4 +90,22 @@ public class PowerUpManager : MonoBehaviour
         GameManager.RemovePowerUp(gameObject.GetInstanceID());
         Destroy(gameObject);
     }
+
+
+     IEnumerator FoodMagnetPowerUp(Collider other){
+
+         //create magnet
+        var magnet = Instantiate(FoodMagnet);
+        magnet.transform.localPosition = other.gameObject.transform.position;
+        magnet.transform.localScale = new Vector3(FoodMagnetScale,FoodMagnetScale,FoodMagnetScale);
+        magnet.transform.parent = other.transform;
+
+        magnet.GetComponent<FoodMagnetPowerUP>().MagnetStrength = FoodMagnetSpeed;
+
+        yield return new WaitForSeconds(duration);
+
+
+        Destroy(magnet);
+        Destroy(gameObject);
+     }
 }
