ef0b747e49952c9db0e3aabcd5ce16168579c734
Ehsan Soltani Abhari <esoltaniabha@student.unimelb.edu.au>
2021-10-16 01:28:13 +1100

    Base sphere for the microbe body is now generated procedurally

---
+++ Petri-fied/Assets/Scripts/MicrobeShapes/MicrobeCore.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Creates the core "body" of a microbe. Inspired by Sebastian League's planet generation tutorial
+// https://www.youtube.com/watch?v=QN39W020LqU   Title: "Procedural Planets"
+public class MicrobeCore : MonoBehaviour
+{
+  [SerializeField, Range(2, 100)]
+  private int faceDetailLevel = 8;
+  [SerializeField, HideInInspector]
+  // The mesh filters for each face
+  private MeshFilter[] filters;
+  // Each face of the original cube
+  private MicrobeFace[] microbeFaces;
+
+  // A cube has 6 faces
+  private const int numFaces = 6;
+  // All directions of each face
+  private Vector3[] dirs = { Vector3.right, Vector3.left, Vector3.down, Vector3.up, Vector3.back, Vector3.forward };
+
+  private void OnValidate()
+  {
+    // Set up the mesh objects
+    setupMeshObjects();
+
+    // Now create the mesh for each mesh object
+    foreach (var face in microbeFaces)
+    {
+      face.createMesh();
+    }
+  }
+
+  // Sets up the mesh objects by adding mesh filters, material and the base empty mesh
+  private void setupMeshObjects()
+  {
+    microbeFaces = new MicrobeFace[numFaces];
+
+    if (filters == null || filters.Length == 0)
+    {
+      filters = new MeshFilter[numFaces];
+    }
+
+    for (int i = 0; i < numFaces; i++)
+    {
+      if (filters[i] == null)
+      {
+        // Create the game object which will hold the face mesh
+        GameObject face = new GameObject("mesh");
+        face.transform.parent = transform;
+
+        // Add a material
+        face.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+        // Add a mesh filter
+        filters[i] = face.AddComponent<MeshFilter>();
+        // Create the base empty mesh
+        filters[i].sharedMesh = new Mesh();
+      }
+
+      // Now create the face itself
+      microbeFaces[i] = new MicrobeFace(filters[i].sharedMesh, faceDetailLevel, dirs[i]);
+    }
+  }
+}
