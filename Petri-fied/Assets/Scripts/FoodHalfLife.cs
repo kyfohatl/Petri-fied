891887cd8d58af88d5594a0154da370bb12d6171
Bodmas12 <80082894+Bodmas12@users.noreply.github.com>
2021-10-27 01:05:48 +1100

    Fixed wrong audio file attached to superfoodeaten and optimised food half life further

--- Petri-fied/Assets/Scripts/FoodHalfLife.cs
+++ Petri-fied/Assets/Scripts/FoodHalfLife.cs
@@ -5,24 +5,25 @@ using UnityEngine;
 public class FoodHalfLife : MonoBehaviour
 {
 	// Time of instantiation and time before self deletion
-	private float timeOfSpawn = 0f;
 	[SerializeField] private float HalfLifeSeconds = 120f; // time in seconds for one half of food to destroy
-
-
-    // Update is called once per frame
-    void Update()
-    {
-		if (Time.time - this.timeOfSpawn >= this.HalfLifeSeconds)
+	
+	// Update is called once per frame
+	void Start()
+	{
+		StartCoroutine("Decompose");
+	}
+	
+	IEnumerator Decompose()
+	{
+		yield return new WaitForSeconds (this.HalfLifeSeconds);
+		if (Random.value > 0.5)
 		{
-			if (Random.value > 0.5)
-			{
-				GameManager.RemoveFood(this.gameObject.GetInstanceID());
-				Destroy(this.gameObject);
-			}
-			else
-			{
-				this.timeOfSpawn = Time.time;
-			}
+			GameManager.RemoveFood(this.gameObject.GetInstanceID());
+			Destroy(this.gameObject);
 		}
-    }
+		else
+		{
+			StartCoroutine("Decompose"); // restart coroutine;
+		}
+	}
 }
