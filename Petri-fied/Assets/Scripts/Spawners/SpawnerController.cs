d0e85d0376657c2c5d59d22f2e8812e6f71dc844
Bodmas12 <80082894+Bodmas12@users.noreply.github.com>
2021-10-31 17:10:44 +1100

    Further additions to the proc spawner

--- Petri-fied/Assets/Scripts/Spawners/SpawnerController.cs
+++ Petri-fied/Assets/Scripts/Spawners/SpawnerController.cs
@@ -6,6 +6,7 @@ public class SpawnerController : MonoBehaviour
 {
 	// Prefab of the desired spawnable object
 	public GameObject prefabToSpawn;
+	private GameObject Arena;
 	
 	// Spawn rate and constraints
 	public float timeBetweenSpawns = 5f; // time in seconds between spawns
@@ -19,19 +20,26 @@ public class SpawnerController : MonoBehaviour
 	protected float arenaRadius;
 	protected Vector3 arenaOrigin;
 	
-	// Start is called before the first frame update
-	void Start()
-	{
-		// Determine current arena dimensions (useful if arena scales in future)
-		getArenaDimensions();
-	}
-	
 	// Function to determine spawner parameters given arena dimensions
 	void getArenaDimensions()
 	{
-		GameObject arena =  GameObject.FindGameObjectWithTag("Arena");
-		this.arenaRadius = arena.GetComponent<ArenaSize>().ArenaRadius;
-		this.arenaOrigin = arena.gameObject.transform.position;
+		if (this.Arena == null)
+		{
+			this.Arena = GameObject.FindWithTag("Arena");
+		}
+		this.arenaRadius = this.Arena.GetComponent<ArenaSize>().ArenaRadius;
+		this.arenaOrigin = this.Arena.transform.position;
+	}
+	
+	// Function to determine how many new objects to spawn in this cycle
+	public int NewSpawnCount(int currentCount)
+	{
+		int spawnCount = Random.Range(this.spawnMin, this.spawnMax + 1);
+		if (currentCount + spawnCount > this.spawnLimit)
+		{
+			spawnCount = this.spawnLimit - this.transform.childCount;
+		}
+		return spawnCount;
 	}
 	
 	// Function to instantiate new prefab objects into the world
@@ -45,7 +53,6 @@ public class SpawnerController : MonoBehaviour
 	}
 	public GameObject Generate(Vector3 spawnPosition)
 	{
-		// Instantiates the newly spawned object and sets as child of spawner
 		GameObject spawned = Instantiate(this.prefabToSpawn, spawnPosition, Random.rotation, transform);
 		return spawned;
 	}
@@ -53,14 +60,17 @@ public class SpawnerController : MonoBehaviour
 	// Function to generate a random position somewhere inside the arena dimensions
 	public Vector3 getRandomPosition() // generic
 	{
+		getArenaDimensions();
 		return Random.insideUnitSphere * this.arenaRadius + this.arenaOrigin;
 	}
 	public Vector3 getRandomPosition(float inRadius) // for specified radius
 	{
+		getArenaDimensions();
 		return Random.insideUnitSphere * inRadius + this.arenaOrigin;
 	}
 	public Vector3 getRandomPosition(float inRadius, Vector3 originPoint) // for specified radius and origin
 	{
+		getArenaDimensions();
 		return Random.insideUnitSphere * inRadius + originPoint;
 	}
 }
